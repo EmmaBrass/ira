74d068abb6d8954d54f8bb59d9c9709e2e919d93
Emma <emma@brassville.com>
2024-09-05 11:43:03 +0100

    deleted files that are now in ira_common

--- ira/face_interaction.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-# Object for saving past interactions that the robot has had with a specific face
-
-class FaceInteraction():
-    def __init__(self, date_time: datetime, outcome: str):
-        self.date_time = date_time
-        self.outcome = outcome
